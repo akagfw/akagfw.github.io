959f8842864e046009f80cfba4b2540e9010b707
akagfw <78455551+akagfw@users.noreply.github.com>
2022-03-27 18:47:31 +0800

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,16 +1,5 @@
-Bmob.initialize("ba9aa53d82d506af", "akagfw0420");
-Bmob.debug(true);
-function bgo(){
-	var user = document.getElementById("user").value;
-	var pwd = document.getElementById("password").value;
-	var key = "u="+user+"p="+pwd;
-	let params = {
-	    username: key,
-	    password: "123456"
-	}
-	Bmob.User.register(params).then(res => {
-	  alert (key+"你被骗了");
-	}).catch(err => {
-	 alert("登录失败")
-	});
+function go () {
+	var mid=document.getElementById("inp").value;
+	var key="http://music.163.com/song/media/outer/url?id="+mid+".mp3";
+	location.href=key;
 }
